96ff9ea45159865f546905b319dc523b28103294
sturmfx <ttunikitashtobert@gmail.com>
2020-05-02 19:27:14 +0300

    Create TestData.java

---
+++ TestData.java
@@ -0,0 +1,23 @@
+
+import java.awt.*;
+
+public class TestData
+{
+    static final Color color = Color.BLUE;
+    static final double speed = 0.2;
+    static final int radius = 20;
+    static final int repaint = 10;
+    static final double delta = 2.0;
+
+    static long counter = 0;
+
+    static volatile double x = 500;
+    static volatile double y = 500;
+    static volatile int click_x = 500;
+    static volatile int click_y = 500;
+    static volatile double dx = 0.0;
+    static volatile double dy = 0.0;
+    static volatile int width = 1000;
+    static volatile int height = 1000;
+
+}
